ab090dcd3ed53792039434fb005003d57e78ac8f
Matthewd340 <matthewd340@nycstudents.net>
2024-03-28 08:51:13 -0400

    Added a new scene with a new NPC and new sound effects for shooting and walking

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -4,20 +4,157 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+
+ public float speed = 3.0f;
+
+  public int maxHealth = 5;
+  public float timeInvincible = 2.0f;
+
+    AudioSource audioSource;
+    public AudioClip hitSound;
+
+  public int health { get { return currentHealth; }}
+  public int currentHealth;
+  public bool isMoving;
+  public AudioClip walkSound;
+
+  bool isInvincible;
+  float invincibleTimer;
+   
+    Rigidbody2D rigidbody2d;
+    float horizontal;
+    float vertical;
+
+    Animator animator;
+    Vector2 lookDirection = new Vector2(1,0);
+
+    public GameObject projectilePrefab;
+
+
     // Start is called before the first frame update
     void Start()
     {
- 
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
+        currentHealth = maxHealth;
+        animator = GetComponent<Animator>();
+
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void PlaySound(AudioClip clip)
+    {
+        audioSource.PlayOneShot(clip);
     }
 
     // Update is called once per frame
     void Update()
-    {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+    {//This code is for moving the player
+         horizontal = Input.GetAxis("Horizontal");
+         vertical = Input.GetAxis("Vertical");
         Vector2 position = transform.position;
-        position.x = position.x + 16.0f * horizontal * Time.deltaTime;
-        position.y = position.y + 16.0f * vertical * Time.deltaTime;
+        position.x = position.x + speed * horizontal * Time.deltaTime;
+        position.y = position.y + speed * vertical * Time.deltaTime;
         transform.position = position;
+
+        Vector2 move = new Vector2(horizontal, vertical);
+
+        if(!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+
+        animator.SetFloat("Look X", lookDirection.x);
+        animator.SetFloat("Look Y", lookDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
+
+        if(horizontal !=0 || vertical !=0)
+        {
+          isMoving = true;
+               
+        }
+        else
+        {
+        isMoving = false;
+         }
+
+        if(isMoving == true && !audioSource.isPlaying)
+        {
+            PlaySound(walkSound);
+        }
+        if(isMoving == false)
+        {
+            audioSource.Stop();
+        }
+
+        if (isInvincible)
+        {
+            invincibleTimer -= Time.deltaTime;
+            if (invincibleTimer < 0)
+                isInvincible = false;
+        }
+
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+            Launch();
+        }
+        //------------------------------------Activate Dialog------------------------------------------
+        if(Input.GetKeyDown(KeyCode.X))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(rigidbody2d.position + Vector2.up * 0.2f, lookDirection, 1.5f, LayerMask.GetMask("NPC"));
+            if (hit.collider != null)
+            {
+                NonPlayerCharacter character = hit.collider.GetComponent<NonPlayerCharacter>();
+                if (character != null)
+                {
+                    character.DisplayDialog();
+                }
+            }
+        }
     }
+
+
+    void FixedUpdate()
+    {
+         horizontal = Input.GetAxis("Horizontal");
+         vertical = Input.GetAxis("Vertical");
+        Vector2 position = transform.position;
+        position.x = position.x + 8.0f * horizontal * Time.deltaTime;
+        position.y = position.y + 8.0f * vertical * Time.deltaTime;
+        transform.position = position;
+
+        rigidbody2d.MovePosition(position);
+    }
+
+    public void ChangeHealth(int amount)
+    {
+        if (amount < 0)
+        {
+            if (isInvincible)
+            return;
+
+            isInvincible = true;invincibleTimer = timeInvincible;
+
+            PlaySound(hitSound);
+        }
+
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        
+        UIHealthBar.instance.SetValue(currentHealth / (float)maxHealth);
+    }
+
+    void Launch()
+    {
+        GameObject projectileObject = Instantiate(projectilePrefab, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
+        
+        Projectile projectile = projectileObject.GetComponent<Projectile>();
+        projectile.Launch(lookDirection, 2000);
+
+        animator.SetTrigger("Launch");
+
+
+    }
+
+
 }
